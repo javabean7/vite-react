6ab27e8356e0dd2a3a653357e2ce22585d856c21
javabean7 <java.c@live.com>
2023-05-24 21:46:41 +0800

    1 file modified, 1 added

---
+++ src/code1.js
@@ -0,0 +1,12 @@
+let res;
+console.info('hello world');
+console.info(1111);
+let arr = [];
+res = { a: 1 };
+res.b = 1;
+let bbbb = Object.create(res);
+console.info(bbbb);
+console.info('res = ', res);
+res = JSON.stringify(res);
+
+export default res;
